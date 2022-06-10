45d08cc62d95df1f815d29396222c7632b55a908
AntonioScarnera <scarnerantonio07@gmail.com>
2022-06-10 15:58:52 +0200

    header

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.base')
+
+
+
+@section('pageTitle')
+    La molisana - Home
+@endsection
+
+@section('mainContent')
+    <h1>Home</h1>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,63 @@
+<header>
+    <div class="header-line">
+
+    </div>
+    <nav class="container">
+        <div>
+            <img src="./images/favicon.ico" alt="">
+        </div>
+        <ul>
+            <li>
+                <a href="">
+                    CHARACTER
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    COMICS
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    MOVIES
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    TV
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    GAMES
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    COLLECTIBLES
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    VIDEOS
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    FANS
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    NEWS
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    SHOP 
+                </a>
+            </li>
+        </ul>
+        <input type="text" size="15" placeholder="Search">
+    </nav>
+</header>
