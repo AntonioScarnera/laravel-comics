893f9190c8c12bd5bd149a926f4820a48d3e8ca9
AntonioScarnera <scarnerantonio07@gmail.com>
2022-06-11 12:32:03 +0200

    conclusione footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,124 @@
 <footer>
-    Made with love by #59
+    <section class="container footer-section">
+        <div class="list-container">
+            <div class="list-item">
+                <h3>DC COMICS</h3>
+                <ul>
+                    <li>
+                        <a href="">Characters</a>
+                    </li>
+                    <li>
+                        <a href="">Comics</a>
+                    </li>
+                    <li>
+                        <a href="">Movies</a>
+                    </li>
+                    <li>
+                        <a href="">TV</a>
+                    </li>
+                    <li>
+                        <a href="">Games</a>
+                    </li>
+                    <li>
+                        <a href="">Videos</a>
+                    </li>
+                    <li>
+                        <a href="">News</a>
+                    </li>
+                </ul>
+                <h3>SHOP</h3>
+                <ul>
+                    <li>
+                        <a href="">Shop DC</a>
+                    </li>
+                    <li>
+                        <a href="">Shop DC Collectibles</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="list-item">
+                <h3>DC</h3>
+                <ul>
+                    <li>
+                        <a href="">Terms Of Use</a>
+                    </li>
+                    <li>
+                        <a href="">Privacy Policy (New)</a>
+                    </li>
+                    <li>
+                        <a href="">Ad Choises</a>
+                    </li>
+                    <li>
+                        <a href="">Advertising</a>
+                    </li>
+                    <li>
+                        <a href="">Jobs</a>
+                    </li>
+                    <li>
+                        <a href="">Subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="">Talent Workshops</a>
+                    </li>
+                    <li>
+                        <a href="">CPSC Certificates</a>
+                    </li>
+                    <li>
+                        <a href="">Ratings</a>
+                    </li>
+                    <li>
+                        <a href="">Shop Help</a>
+                    </li>
+                    <li>
+                        <a href="">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="list-item">
+                <h3>SITES</h3>
+                <ul>
+                    <li>
+                        <a href="">DC</a>
+                    </li>
+                    <li>
+                        <a href="">MAD Magazine</a>
+                    </li>
+                    <li>
+                        <a href="">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="">DC Power Visa</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="list-pie">
+                <p>All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <span>noted here</span>. All rights reserved. <span>Cookies Settings</span></p>
+            </div>
+        </div>
+
+        <div class="big-logo">
+
+        </div>
+    </section>
+    <section class="footer-pie">
+        <div class="footer-pie-container container">
+            <div class="footer-btn">
+                SIGN-UP NOW!
+            </div>
+            <div class="footer-socials">
+                <ul>
+                    <li><span>FOLLOW US</span></li>
+                    <li><img src="./images/footer-facebook.png" alt=""></li>
+                    <li><img src="./images/footer-twitter.png" alt=""></li>
+                    <li><img src="./images/footer-youtube.png" alt=""></li>
+                    <li><img src="./images/footer-pinterest.png" alt=""></li>
+                    <li><img src="./images/footer-periscope.png" alt=""></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
 </footer>
